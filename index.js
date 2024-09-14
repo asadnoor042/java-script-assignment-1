42eb7253a194c704ba5cb0089f484522061f4d7f
asadnoor042 <asadnoor7342@gmail.com>
2024-09-14 04:25:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+               chap # 05
+ // question # 01/02
+ var +Number(num1 = )+prompt ("enter first number")
+ var num2 = +prompt ("enter second number")
+ document.write("the sum of " + +Number(num1 + )" and " + num2 + " is " +Number(+Number(num1 + )num2))
+ 
+ document.write("the difference of " + +Number(num1 + )" and " + num2 + " is " +Number(+Number(num1 - )num2))
+ 
+ document.write("the product of " + +Number(num1 + )" and " + num2 + " is " +Number(+Number(num1 * )num2))
+ 
+ document.write("the division of " + +Number(num1 + )" and " + num2 + " is " +Number(+Number(num1 / )num2))
+ 
+ document.write("the modulus of " + +Number(num1 + )" and " + num2 + " is " +Number(+Number(num1 % )num2))
+ 
+ // question # 03
+ var new_number
+ document.write("“Value after variable declaration is " + new_number)
+ new_number = 5
+ document.write(" initial value " + new_number)
+ ++new_number
+ document.write(" value after increament is " + new_number)
+ new_number = new_number + 7
+ document.write(" value after addition  is " + new_number)
+ --new_number 
+ document.write(" value after decreament  is " + new_number)
+ new_number2 = 3
+ document.write('the reminder is ' + Number(new_number % new_number2))
+ 
+ // question # 04
+ var ticket_price = 600
+ var number_of_tickets = 5
+ document.write("total cost to buy " + number_of_tickets + " tickets to a movie is " +Number (ticket_price * number_of_tickets))
+ // question # 05
+ var num1 = 4;
+ document.write("<pre>Table Of 4\n");
+ document.write("4*1 = " + Number(num1 * 1) + "\n");
+ document.write("4*2 = " + Number(num1 * 2) + "\n");
+ document.write("4*3 = " + Number(num1 * 3) + "\n");
+ document.write("4*4 = " + Number(num1 * 4) + "\n");
+ document.write("4*5 = " + Number(num1 * 5) + "\n");
+ document.write("4*6 = " + Number(num1 * 6) + "\n");
+ document.write("4*7 = " + Number(num1 * 7) + "\n");
+ document.write("4*8 = " + Number(num1 * 8) + "\n");
+ document.write("4*9 = " + Number(num1 * 9) + "\n");
+ document.write("4*10 = " + Number(num1 * 10) + "\n</pre>");
+ 
+  // Question 6
+         
+         var celsius = 31;
+         var fahrenheit = 90;
+         
+         var CF = (celsius * 9) / 5 + 32;
+         var FC = ((fahrenheit - 32) * 5) / 9;
+         
+         document.write(celsius + "C is " + CF + "F" + "<br/>");
+         document.write(fahrenheit + "F is " + FC + "C" + "<br/><br/><br/>");
